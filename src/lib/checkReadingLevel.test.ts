eee6ff1608cb0ed229c6691e4aa81cf58921e9a7
Tyler Krys <tylerkrys@gmail.com>
2024-02-17 19:11:39 -0800

    Add getReadingLevelDataFromParagraph() and test

--- src/lib/checkReadingLevel.test.ts
+++ src/lib/checkReadingLevel.test.ts
@@ -1,10 +1,11 @@
 import { describe, it, expect } from "vitest";
 import {
-  getTextFromHtmlParagraph,
+  getReadingLevelDataFromParagraph,
   getSentencesFromParagraph,
+  getTextFromHtmlParagraph,
 } from "./checkReadingLevel";
 
-describe("getTextFromHtmlParagraph", () => {
+describe("getTextFromHtmlParagraph()", () => {
   it("returns the inner text from a string of paragraph HTML", () => {
     const htmlParagraph = "<p>Hello world.</p>";
     const text = getTextFromHtmlParagraph(htmlParagraph);
@@ -13,7 +14,7 @@ describe("getTextFromHtmlParagraph", () => {
   });
 });
 
-describe("getSentencesFromParagraph", () => {
+describe("getSentencesFromParagraph()", () => {
   it("returns an array of sentences from a string of paragraph text", () => {
     const paragraph = "Hello world. The quick brown fox? The end!";
     const sentences = getSentencesFromParagraph(paragraph);
@@ -22,5 +23,17 @@ describe("getSentencesFromParagraph", () => {
     expect(sentences[0]).toEqual("Hello world");
     expect(sentences[1]).toEqual("The quick brown fox");
     expect(sentences[2]).toEqual("The end");
+  });
+});
+
+describe("getReadingLevelDataFromParagraph()", () => {
+  it("returns an object of reading ease level data for a paragraph", () => {
+    const paragraph = "Hello world. Orange you glad I didn't say banana? End!";
+    const data = getReadingLevelDataFromParagraph(paragraph);
+
+    expect(data.readingEase).toEqual("76.55");
+    expect(data.readingEaseSchoolLevel).haveOwnProperty("grade");
+    expect(data.readingEaseSchoolLevel?.grade).toEqual("8-9");
+    expect(data.readingGrade).toEqual("3.41");
   });
 });
--- src/lib/checkReadingLevel.ts
+++ src/lib/checkReadingLevel.ts
@@ -1,3 +1,8 @@
+import BigNumber from "bignumber.js";
+import { syllable } from "syllable";
+
+import { readingEaseLevels, ReadingEase } from "./readingEaseLevels";
+
 type HTMLParagraph = `<p>${string}</p>`;
 
 export function getTextFromHtmlParagraph(htmlParagraph: HTMLParagraph): string {
@@ -17,4 +22,83 @@ export function getSentencesFromParagraph(paragraph: string): string[] {
     .split(punctuationRegex)
     .filter((s) => s !== "")
     .map((s) => s.trim());
+}
+
+interface SentenceData {
+  sentence: string;
+  characters: number;
+  words: number;
+  syllables: number;
+}
+
+interface ReadingLevel {
+  readingEase: string;
+  readingEaseSchoolLevel: ReadingEase | undefined;
+  readingGrade: string;
+  characterCount: number;
+  wordCount: number;
+  sentenceCount: number;
+  syllableCount: number;
+  sentences: SentenceData[];
+}
+
+export function getReadingLevelDataFromParagraph(
+  paragraph: string
+): ReadingLevel {
+  const characterCount = paragraph.length;
+  const sentences = getSentencesFromParagraph(paragraph);
+  const sentenceCount = sentences.length;
+
+  // Word count for the entire paragraph
+  let wordCount = 0;
+  sentences.forEach((sentence) => (wordCount += sentence.split(" ").length));
+
+  const sentencesData: SentenceData[] = [];
+  let syllableCount = 0;
+
+  sentences.forEach((sentence) => {
+    syllableCount += syllable(sentence);
+    const words = sentence.split(" ");
+
+    const data = {
+      sentence,
+      characters: sentence.length,
+      words: words.length,
+      syllables: syllable(sentence),
+    };
+
+    sentencesData.push(data);
+  });
+
+  // Flesch reading ease
+  const wordsOverSentences = BigNumber(wordCount).dividedBy(sentenceCount);
+  const syllablesOverWords = BigNumber(syllableCount).dividedBy(wordCount);
+  const easeSentenceLengthScore =
+    BigNumber(1.015).multipliedBy(wordsOverSentences);
+  const easeSyllableScore = BigNumber(84.6).multipliedBy(syllablesOverWords);
+  const readingEase = BigNumber(206.835)
+    .minus(easeSentenceLengthScore)
+    .minus(easeSyllableScore);
+  const readingEaseSchoolLevel = readingEaseLevels.find((level) =>
+    readingEase.isGreaterThanOrEqualTo(level.max)
+  );
+
+  // Flesch-Kincaid grade level
+  const gradeSentenceLengthScore =
+    BigNumber(0.39).multipliedBy(wordsOverSentences);
+  const gradeSyllableScore = BigNumber(11.8).multipliedBy(syllablesOverWords);
+  const readingGrade = BigNumber(gradeSentenceLengthScore)
+    .plus(gradeSyllableScore)
+    .minus(15.59);
+
+  return {
+    readingEase: readingEase.toFixed(2),
+    readingEaseSchoolLevel,
+    readingGrade: readingGrade.toFixed(2),
+    characterCount,
+    wordCount,
+    sentenceCount,
+    syllableCount,
+    sentences: sentencesData,
+  };
 }
